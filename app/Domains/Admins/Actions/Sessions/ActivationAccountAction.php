8a11d9e2d0f4c6cccb2e3f379ce5442213dbfa1b
Mohammed Zubair <moh.775123554@gmail.com>
2023-06-05 21:58:25 +0300

    Add activiton account action with handle functionality

---
+++ app/Domains/Admins/Actions/Sessions/ActivationAccountAction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Domains\Admins\Actions\Sessions;
+
+use App\Http\Requests\Admins\Sessions\ActivateAccountRequest;
+use App\Models\OtpActivation;
+use Illuminate\Support\Carbon;
+
+class ActivationAccountAction
+{
+    public function __invoke(ActivateAccountRequest $request)
+    {
+        $otpActivation = OtpActivation::query()->where('otp', $request->otp)->firstOrFail();
+
+        if (Carbon::parse($otpActivation->created_at)->addHours(24)->isPast()) {
+            // do someting
+        }
+
+        $admin = $otpActivation->activatable;
+
+        if ($admin->isActivatedAccount()) {
+            //do sum thing
+        }
+
+        $admin->setAsActivateAccount();
+
+        return $admin->email;
+        
+    }
+}
