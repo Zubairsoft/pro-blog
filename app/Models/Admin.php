e1972af44a7bf919065262c4b1110d15dc7cd0ff
Mohammed Zubair <moh.775123554@gmail.com>
2023-06-05 22:11:40 +0300

    Update admin  model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,15 +2,32 @@
 
 namespace App\Models;
 
+use App\Models\ModelAttributes\AdminAttributes;
+use App\Models\ModelEloquent\AdminEloquent;
+use Domains\Supports\Traits\ActivateAccount;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+
+class Admin extends Authenticatable  implements HasMedia
 {
-    use HasApiTokens, HasFactory, Notifiable,HasUuids;
+    use
+        HasApiTokens,
+        HasFactory,
+        Notifiable,
+        HasUuids,
+        InteractsWithMedia,
+        ActivateAccount,
+        AdminEloquent,
+        AdminAttributes,
+        HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +39,7 @@ class Admin extends Model
         'email',
         'password',
         'email_verified_at',
+        'local',
         'is_active',
     ];
 
@@ -41,6 +59,13 @@ class Admin extends Model
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'is_active'=>'boolean',
+        'is_active' => 'boolean',
     ];
+
+    public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection('avatar')
+            ->singleFile();
+    }
 }
