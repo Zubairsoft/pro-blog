11457d88f968bd54228dd89c2e2c374d9559218c
Mohammed Zubair <moh.775123554@gmail.com>
2023-06-11 20:51:21 +0300

    Update migration
    update user migration 
    update author migration
    update admin migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,13 +16,12 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('name');
+            $table->tinyInteger('gender');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('local')->default(LocalEnum::ARABIC);
-            $table->boolean('is_active')->default(false);
-            //$table->rememberToken();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
