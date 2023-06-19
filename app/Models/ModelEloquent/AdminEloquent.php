0a5bf26af78e0962ef0831bda1fb61ed0b71abae
Mohammed Zubair <moh.775123554@gmail.com>
2023-06-19 23:06:18 +0300

    Update admin eloquent

--- app/Models/ModelEloquent/AdminEloquent.php
+++ app/Models/ModelEloquent/AdminEloquent.php
@@ -3,6 +3,8 @@
 namespace App\Models\ModelEloquent;
 
 use App\Models\OtpActivation;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 trait AdminEloquent
@@ -10,5 +12,10 @@ trait AdminEloquent
     public function otpActivation(): MorphOne
     {
         return $this->morphOne(OtpActivation::class, 'activatable');
+    }
+
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Tag::class);
     }
 }
