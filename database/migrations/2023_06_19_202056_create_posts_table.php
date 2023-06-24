8a70ec457bdc7037ca9845bf967f8d18875e9d9f
Mohammed Zubair <moh.775123554@gmail.com>
2023-06-24 10:48:51 +0300

    Add posts table with handle functionality

---
+++ database/migrations/2023_06_19_202056_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuidMorphs('authorable');
+            $table->string('title_ar');
+            $table->string('title_en')->nullable();
+            $table->text('description_ar');
+            $table->text('description_en')->nullable();
+            $table->boolean('is_publish_ar')->default(false);
+            $table->boolean('is_publish_en')->default(false);
+            $table->boolean('status')->default(false);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
