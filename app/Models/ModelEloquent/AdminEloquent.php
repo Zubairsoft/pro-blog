4e8a9edaa1e6c9204aead1839b789264e1505cb5
Mohammed Zubair <moh.775123554@gmail.com>
2023-06-24 10:51:44 +0300

    Update admin and author trait with handle functionality

--- app/Models/ModelEloquent/AdminEloquent.php
+++ app/Models/ModelEloquent/AdminEloquent.php
@@ -3,8 +3,10 @@
 namespace App\Models\ModelEloquent;
 
 use App\Models\OtpActivation;
+use App\Models\Post;
 use App\Models\Tag;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 trait AdminEloquent
@@ -12,6 +14,11 @@ trait AdminEloquent
     public function otpActivation(): MorphOne
     {
         return $this->morphOne(OtpActivation::class, 'activatable');
+    }
+
+    public function posts():MorphMany
+    {
+        return $this->morphMany(Post::class,'authorable');
     }
 
     public function tags(): HasMany
