fa31c0083f0e9b47174fef7cff0817aef52c5731
Mohammed Zubair <moh.775123554@gmail.com>
2023-06-26 14:32:14 +0300

    Update admin login action add specification check

--- app/Domains/Admins/Actions/Sessions/LoginAdminAction.php
+++ app/Domains/Admins/Actions/Sessions/LoginAdminAction.php
@@ -5,6 +5,8 @@ namespace Domains\Admins\Actions\Sessions;
 use App\Exceptions\CustomException\LogicException;
 use App\Http\Requests\Admins\Sessions\LoginAdminRequest;
 use App\Models\Admin;
+use Domains\Admins\Specification\Admin\IsActiveSpecification;
+use Domains\Admins\Specification\Admin\IsVerifiedEmailSpecification;
 use Illuminate\Support\Facades\Auth;
 
 class LoginAdminAction
@@ -12,15 +14,19 @@ class LoginAdminAction
     public function __invoke(LoginAdminRequest $request)
     {
         Auth::shouldUse(config('auth.admin-web-guard'));
-        
+
         if (!Auth::attempt($request->validated())) {
             throw new LogicException(__('auth.failed'), 422);
         }
 
         $admin = Auth::user();
 
-        if (!$admin->isActivatedAccount()) {
+        if (!(new IsVerifiedEmailSpecification())->isAllowed($admin)) {
             throw new LogicException(__('auth.verify_email'), 422);
+        }
+
+        if (!(new IsActiveSpecification())->isAllowed($admin)) {
+            throw new LogicException(__('exceptions.is_not_active_account'), 422);
         }
 
         $admin['token'] = $admin->createToken('adminAccessToken')->plainTextToken;
