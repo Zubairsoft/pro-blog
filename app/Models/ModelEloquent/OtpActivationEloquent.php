2c62adc5dc755a81fe4740445008e2122ddc376d
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-08 14:08:41 +0300

    Update Author model

--- app/Models/ModelEloquent/OtpActivationEloquent.php
+++ app/Models/ModelEloquent/OtpActivationEloquent.php
@@ -2,12 +2,8 @@
 
 namespace App\Models\ModelEloquent;
 
-use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 trait OtpActivationEloquent
 {
-    public function activatable(): MorphTo
-    {
-        return $this->morphTo();
-    }
+ 
 }
