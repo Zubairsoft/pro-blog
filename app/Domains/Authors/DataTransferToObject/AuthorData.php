af8d96680615edbbac9e555baa76871c94d83ce9
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-08 13:00:50 +0300

    Add author data

---
+++ app/Domains/Authors/DataTransferToObject/AuthorData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Domains\Authors\DataTransferToObject;
+
+use Spatie\LaravelData\Data;
+
+class AuthorData extends Data
+{
+    public function __construct(
+        public ?string $first_name,
+        public ?string $last_name,
+        public ?string $email,
+        public ?string $password,
+        public ?bool $is_active,
+        public ?int $gender,
+        public ?bool $local
+    ) {
+    }
+
+    public static function fromRequest($request)
+    {
+        return new self(
+            $request->post('first_name'),
+            $request->post('last_name'),
+            $request->post('email'),
+            $request->post('password'),
+            $request->post('is_active'),
+            $request->post('gender'),
+            $request->post('local'),
+        );
+    }
+}
