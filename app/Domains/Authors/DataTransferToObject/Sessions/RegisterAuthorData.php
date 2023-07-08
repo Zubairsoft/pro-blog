56436bb0a9bf1c8311779ddd1bfe1f5f68adc7b2
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-08 13:03:47 +0300

    Add register author data with handle functionaliy

---
+++ app/Domains/Authors/DataTransferToObject/Sessions/RegisterAuthorData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Domains\Authors\DataTransferToObject\Sessions;
+
+use Spatie\LaravelData\Data;
+
+class RegisterAuthorData extends Data
+{
+    public function __construct(
+        public string $first_name,
+        public string $last_name,
+        public string $email,
+        public string $password,
+        public int $gender,
+        public ?bool $local
+    ) {
+    }
+
+    public static function fromRequest($request)
+    {
+        return new self(
+            $request->post('first_name'),
+            $request->post('last_name'),
+            $request->post('email'),
+            $request->post('password'),
+            $request->post('gender'),
+            $request->post('local'),
+        );
+    }
+}
