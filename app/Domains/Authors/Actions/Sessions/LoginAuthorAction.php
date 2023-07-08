aa9fd578663f672a54f05bf776205131154719e1
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-08 12:59:51 +0300

    Add login Author Action with handle functionality

---
+++ app/Domains/Authors/Actions/Sessions/LoginAuthorAction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Domains\Authors\Actions\Sessions;
+
+use App\Exceptions\CustomException\LogicException;
+use App\Http\Requests\Authors\Sessions\loginRequest;
+use Domains\Authors\Specification\IsActiveSpecification;
+use Domains\Authors\Specification\IsVerifiedEmailSpecification;
+use Illuminate\Support\Facades\Auth;
+
+class LoginAuthorAction
+{
+    public function __invoke(loginRequest $request)
+    {
+        Auth::shouldUse(config('auth.author-web-guard'));
+
+        if (!Auth::attempt($request->validated())) {
+            return sendSuccessResponse(__('auth.failed'));
+        }
+
+        $author = Auth::user();
+
+        if (!(new IsVerifiedEmailSpecification)->isAllowed($author)) {
+            throw new LogicException(__('auth.verify_email'), 422);
+        }
+
+        if (!(new IsActiveSpecification)->isAllowed($author)) {
+
+            throw new LogicException(__('auth.not_activated_account'), 422);
+        }
+
+        $author['token'] = $author->createToken('authorAccessToken')->plainTextToken;
+
+        return $author;
+    }
+}
