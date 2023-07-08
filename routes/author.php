244ddb24c95c119351296a88ad2999cc7d35791d
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-08 14:12:02 +0300

    Add api route endpoint

--- routes/author.php
+++ routes/author.php
@@ -1,1 +1,15 @@
 <?php
+
+use App\Http\Controllers\Api\v1\Authors\Sessions\SessionController;
+use Illuminate\Support\Facades\Route;
+
+Route::name('sessions.')->prefix('sessions')->group(function () {
+    Route::post('/register', [SessionController::class, 'register'])->name('register');
+    Route::post('/login', [SessionController::class, 'login'])->name('login');
+    Route::post('/send-verification-code', [SessionController::class, 'sendVerificationCode'])->name('send_verification_code');
+    Route::post('/activate-account', [SessionController::class, 'activateAccount'])->name('activate_account');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [SessionController::class, 'logout'])->name('logout');
+    });
+});
