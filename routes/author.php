b13f0c033bc4c5bc292a1d60ee23ba21911ef60c
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-10 22:41:47 +0300

    Add api route with handle functionality

--- routes/author.php
+++ routes/author.php
@@ -8,6 +8,8 @@ Route::name('sessions.')->prefix('sessions')->group(function () {
     Route::post('/login', [SessionController::class, 'login'])->name('login');
     Route::post('/send-verification-code', [SessionController::class, 'sendVerificationCode'])->name('send_verification_code');
     Route::post('/activate-account', [SessionController::class, 'activateAccount'])->name('activate_account');
+    Route::post('rest-password',[SessionController::class,'sendRestPassword'])->name('send_rest_password');
+    Route::patch('rest-password',[SessionController::class,'restPassword'])->name('rest_password');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [SessionController::class, 'logout'])->name('logout');
