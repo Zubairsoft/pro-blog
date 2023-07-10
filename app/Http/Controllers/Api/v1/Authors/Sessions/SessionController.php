8b074dc12db2c6d13331dc9c52dc13fa5a359ad0
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-10 22:41:23 +0300

    Update session controller add send and rest password functions

--- app/Http/Controllers/Api/v1/Authors/Sessions/SessionController.php
+++ app/Http/Controllers/Api/v1/Authors/Sessions/SessionController.php
@@ -6,11 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Authors\Sessions\ActivateAccountRequest;
 use App\Http\Requests\Authors\Sessions\loginRequest;
 use App\Http\Requests\Authors\Sessions\RegisterAuthorRequest;
+use App\Http\Requests\Authors\Sessions\ResetPasswordRequest;
+use App\Http\Requests\Authors\Sessions\SendResetPasswordRequest;
 use App\Http\Requests\Authors\Sessions\SendVerificationCodeRequest;
 use Domains\Authors\Actions\Sessions\LogoutAuthorAction;
 use Domains\Authors\Actions\Sessions\ActivateAccountAction;
 use Domains\Authors\Actions\Sessions\LoginAuthorAction;
 use Domains\Authors\Actions\Sessions\RegisterAuthorAction;
+use Domains\Authors\Actions\Sessions\ResetPasswordAction;
+use Domains\Authors\Actions\Sessions\SendRestPasswordAction;
 use Domains\Authors\Actions\Sessions\SendVerificationCodeAction;
 
 class SessionController extends Controller
@@ -41,6 +45,20 @@ class SessionController extends Controller
         $activateAccount = (new ActivateAccountAction)($request);
 
         return sendSuccessResponse(__('auth.account_verified'), $activateAccount);
+    }
+
+    public function sendRestPassword(SendResetPasswordRequest $request)
+    {
+         $restPassword=(new SendRestPasswordAction)($request);
+
+         return sendSuccessResponse(__('passwords.send'),$restPassword);
+    }
+
+    public function restPassword(ResetPasswordRequest $request)
+    {
+       (new ResetPasswordAction)($request);
+
+       return sendSuccessResponse(__('passwords.reset'));
     }
 
     public function logout()
