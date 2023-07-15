25a94f2a4dc1a5a1fcc9e5136062fea07a906de1
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-15 19:04:21 +0300

    Update post request handle functionality

--- app/Http/Requests/Authors/PostRequest.php
+++ app/Http/Requests/Authors/PostRequest.php
@@ -4,7 +4,8 @@ namespace App\Http\Requests\Authors;
 
 use Domains\Supports\Traits\Http\HttpRequest;
 use Illuminate\Foundation\Http\FormRequest;
-
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\File;
 
 class PostRequest extends FormRequest
 {
@@ -28,19 +29,110 @@ class PostRequest extends FormRequest
 
     }
 
-    
     public function creationRules(): array
     {
-        return [];
+        return [
+            'title_ar' => [
+                'required',
+                'min:3',
+                'max:255',
+            ],
+            'title_en' => [
+                'required',
+                'min:3',
+                'max:255',
+            ],
+            'description_ar' => [
+                'required',
+                'min:3',
+                'max:1000',
+            ],
+            'description_en' => [
+                'required',
+                'min:3',
+                'max:1000',
+            ],
+            'poster' => [
+                'required',
+                File::types('jpeg')->max(1024)
+            ],
+            'images' => [
+                'array'
+            ],
+            'images.*' => [
+                'required',
+                File::types('jpeg')->max(1024)
+            ],
+            'is_published_ar' => [
+                'boolean',
+            ],
+            'is_published_en' => [
+                'boolean',
+            ],
+            'tags' => [
+                'required',
+                'array'
+            ],
+            'tags.*'=>[
+                'required',
+                Rule::exists('tags','id')
+            ]
+        ];
     }
 
     public function updatingRules(): array
     {
-        return [];
+        return [
+            'title_ar' => [
+                'min:3',
+                'max:255',
+            ],
+            'title_en' => [
+                'min:3',
+                'max:255',
+            ],
+            'description_ar' => [
+                'min:3',
+                'max:1000',
+            ],
+            'description_en' => [
+                'min:3',
+                'max:1000',
+            ],
+            'poster' => [
+                File::types('jpeg')->max(1024)
+            ],
+            'images' => [
+                'array'
+            ],
+            'images.*' => [
+                'required',
+                File::types('jpeg')->max(1024)
+            ],
+            'status' => [
+                'boolean',
+            ],
+            'tags' => [
+                'array'
+            ],
+            'tags.*'=>[
+                Rule::exists('tags','id')
+            ]
+
+        ];
     }
 
     public function DeletionRules(): array
     {
-        return [];
+        return [
+            'ids' => [
+                'required',
+                'array'
+            ],
+            'ids.*' => [
+                'required',
+                Rule::exists('posts', 'id')
+            ]
+        ];
     }
 }
