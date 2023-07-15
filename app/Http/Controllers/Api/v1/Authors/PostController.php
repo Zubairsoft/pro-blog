e103b89b18081a7c3cd752d712bb17b727f59caa
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-15 16:31:59 +0300

    Add post controller

---
+++ app/Http/Controllers/Api/v1/Authors/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Authors;
+
+use App\Http\Controllers\Controller;
+use Domains\Authors\Actions\Posts\IndexPostAction;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+}
