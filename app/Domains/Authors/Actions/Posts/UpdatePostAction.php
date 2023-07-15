6209826e12da7007e014e3f344087e40eac19f8d
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-15 18:01:30 +0300

    Update update post action with handle functionality

--- app/Domains/Authors/Actions/Posts/UpdatePostAction.php
+++ app/Domains/Authors/Actions/Posts/UpdatePostAction.php
@@ -11,6 +11,21 @@ final class UpdatePostAction
 {
     public function __invoke(PostRequest $request, string $id): Post
     {
+        $attributes = unsetArrayEmptyParam(PostData::fromRequest($request)->toArray());
+        $author = Auth::user();
 
+        $post = $author->posts()->findOrFail($id);
+
+        $post->update($attributes);
+
+        if ($request->has('tags')) {
+            $post->tags()->sync($request->tags);
+        }
+
+        if ($request->poster?->isFile()) {
+            $post->addMediaFromRequest('poster')->toMediaCollection('poster');
+        }
+
+        return $post;
     }
 }
