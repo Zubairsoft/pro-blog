5e95f388813095593b640716a02d46a5838292b4
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-15 17:01:09 +0300

    Update post controller add store function with handle functionality

--- app/Http/Controllers/Api/v1/Authors/PostController.php
+++ app/Http/Controllers/Api/v1/Authors/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\v1\Authors;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Authors\PostRequest;
 use Domains\Authors\Actions\Posts\IndexPostAction;
+use Domains\Authors\Actions\Posts\StorePostAction;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -23,4 +25,18 @@ class PostController extends Controller
         return sendSuccessResponse(__('messages.get_data'), $posts);
     }
 
+
+    /**
+     * Handle the incoming request for store post
+     * 
+     * @param PostRequest $request
+     * 
+     * @return JsonResponse
+     */
+    public function store(PostRequest $request): JsonResponse
+    {
+        $post = (new StorePostAction)($request);
+
+        return sendSuccessResponse(__('messages.create_data'), $post);
+    }
 }
