5fb58af4d8c51cbbc5633b96832987363003b3e4
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-15 16:59:39 +0300

    Update store post action handle functionality

--- app/Domains/Authors/Actions/Posts/StorePostAction.php
+++ app/Domains/Authors/Actions/Posts/StorePostAction.php
@@ -11,6 +11,18 @@ class StorePostAction
 {
     public function __invoke(PostRequest $request): Post
     {
+        $attributes = unsetArrayEmptyParam(PostData::fromRequest($request)->toArray());
 
+        $author = Auth::user();
+
+        $post = $author->posts()->create($attributes);
+
+        $post->tags()->sync($request->tags);
+
+        if ($request->poster->isFile()) {
+            $post->addMediaFromRequest('poster')->toMediaCollection('poster');
+        }
+
+        return $post;
     }
 }
