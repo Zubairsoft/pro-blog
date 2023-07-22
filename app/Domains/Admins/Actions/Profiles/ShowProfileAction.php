9e0dc35c895cec9aad671c59d55809fd842de40c
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-22 22:43:32 +0300

    Add show profile action

---
+++ app/Domains/Admins/Actions/Profiles/ShowProfileAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Domains\Admins\Actions\Profiles;
+
+use App\Models\Admin;
+use Illuminate\Support\Facades\Auth;
+
+final class ShowProfileAction
+{
+    public function __invoke(): Admin
+    {
+    }
+}
