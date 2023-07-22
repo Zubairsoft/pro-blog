346800d0ae8241485f867f769698204b8788c6e4
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-22 22:51:53 +0300

    Add profile controller

---
+++ app/Http/Controllers/Api/v1/Admins/Sessions/ProfileController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Admins\Sessions;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admins\ProfileRequest;
+use App\Http\Resources\Admins\Profiles\ProfileResource;
+use Domains\Admins\Actions\Profiles\ShowProfileAction;
+use Domains\Admins\Actions\Profiles\UpdateProfileAction;
+use Illuminate\Http\JsonResponse;
+
+class ProfileController extends Controller
+{
+
+}
