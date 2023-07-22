46b2aee76333a82ddbc79c5eeaf7a7f76b1d6256
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-22 22:45:30 +0300

    Add profile data

---
+++ app/Domains/Admins/DataTransferToObject/ProfileData.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Domains\Admins\DataTransferToObject;
+
+use App\Http\Requests\Admins\ProfileRequest;
+use Spatie\LaravelData\Data;
+
+final class ProfileData extends Data
+{
+
+
+}
