2df96842298b91703731b40afd94b485ef390edd
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-22 22:49:49 +0300

    Update profile resource handle functionality

--- app/Http/Resources/Admins/Profiles/ProfileResource.php
+++ app/Http/Resources/Admins/Profiles/ProfileResource.php
@@ -15,7 +15,11 @@ class ProfileResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-       
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'gender' => $this->gender,
+            'local' => $this->local
         ];
     }
 }
