a11efc6b3bfb3edf14f6f2bb0d9be15197f5e44f
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-24 22:20:12 +0300

    Add profile date

---
+++ app/Domains/Authors/DataTransferToObject/ProfileData.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Domains\Authors\DataTransferToObject;
+
+use App\Http\Requests\Authors\ProfileRequest;
+use Spatie\LaravelData\Data;
+
+final class ProfileData extends Data
+{
+
+   
+}
