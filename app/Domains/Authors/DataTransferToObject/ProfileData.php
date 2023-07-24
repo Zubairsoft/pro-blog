f6df6626a2cf734472d7613f48bf136c0c88ba3b
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-24 22:20:46 +0300

    Update profile data handle functionality

--- app/Domains/Authors/DataTransferToObject/ProfileData.php
+++ app/Domains/Authors/DataTransferToObject/ProfileData.php
@@ -8,5 +8,25 @@ use Spatie\LaravelData\Data;
 final class ProfileData extends Data
 {
 
-   
+    public function __construct(
+        public ?string  $first_name,
+        public ?string  $last_name,
+        public ?string $email,
+        public ?string $password,
+        public ?int $gender,
+        public ?bool $local
+    ) {
+    }
+
+    public static function fromRequest(ProfileRequest $request): ProfileData
+    {
+        return new self(
+            $request->post('first_name'),
+            $request->post('last_name'),
+            $request->post('email'),
+            $request->post('password'),
+            $request->post('gender'),
+            $request->post('local')
+        );
+    }
 }
