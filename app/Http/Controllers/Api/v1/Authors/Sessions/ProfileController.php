1638b1d3ffaccd977e5f8e1cbc951ae2cbf74a48
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-24 22:19:24 +0300

    Add profile controller

---
+++ app/Http/Controllers/Api/v1/Authors/Sessions/ProfileController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Authors\Sessions;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Authors\ProfileRequest;
+use Domains\Authors\Actions\Profiles\UpdateProfileAction;
+use Domains\Authors\Actions\Profiles\ShowProfileAction;
+use Illuminate\Http\JsonResponse;
+
+class ProfileController extends Controller
+{
+
+}
