b2b004c4960ed3aa80b7042794525e0585e2f36e
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-29 20:22:09 +0300

    Add endpoint

--- routes/author.php
+++ routes/author.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\Authors\BookmarkController;
 use App\Http\Controllers\Api\v1\Authors\PostController;
 use App\Http\Controllers\Api\v1\Authors\Sessions\ProfileController;
 use App\Http\Controllers\Api\v1\Authors\Sessions\SessionController;
@@ -10,24 +11,31 @@ Route::name('sessions.')->prefix('sessions')->group(function () {
     Route::post('/login', [SessionController::class, 'login'])->name('login');
     Route::post('/send-verification-code', [SessionController::class, 'sendVerificationCode'])->name('send_verification_code');
     Route::post('/activate-account', [SessionController::class, 'activateAccount'])->name('activate_account');
-    Route::post('rest-password',[SessionController::class,'sendRestPassword'])->name('send_rest_password');
-    Route::patch('rest-password',[SessionController::class,'restPassword'])->name('rest_password');
+    Route::post('rest-password', [SessionController::class, 'sendRestPassword'])->name('send_rest_password');
+    Route::patch('rest-password', [SessionController::class, 'restPassword'])->name('rest_password');
 
     Route::middleware('auth:author-api')->group(function () {
         Route::post('/logout', [SessionController::class, 'logout'])->name('logout');
 
-        
-        Route::get('profile',[ProfileController::class,'show'])->name('show.profile');
-        Route::patch('profile',[ProfileController::class,'update'])->name('update.profile');
+
+        Route::get('profile', [ProfileController::class, 'show'])->name('show.profile');
+        Route::patch('profile', [ProfileController::class, 'update'])->name('update.profile');
     });
 });
 
-Route::middleware('auth:author-api')->group(function(){
+Route::middleware('auth:author-api')->group(function () {
     Route::name('posts.')->prefix('posts')->group(function () {
         Route::get('/', [PostController::class, 'index'])->name('index');
         Route::post('/', [PostController::class, 'store'])->name('store');
         Route::get('/{id}', [PostController::class, 'show'])->name('show');
         Route::patch('/{id}', [PostController::class, 'update'])->name('update');
         Route::delete('/', [PostController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::name('bookmarks.')->prefix('bookmarks')->group(function () {
+        Route::get('/', [BookmarkController::class, 'index'])->name('index');
+        Route::post('/', [BookmarkController::class, 'store'])->name('store');
+        Route::get('/{id}', [BookmarkController::class, 'show'])->name('show');
+        Route::delete('/', [BookmarkController::class, 'destroy'])->name('destroy');
     });
 });
