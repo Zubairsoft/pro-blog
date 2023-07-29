9f7ee21234b76524a2ce109b0f7b1870b897fe23
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-29 20:18:09 +0300

    Update bookmark model

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\ModelEloquent\BookmarkEloquent;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Bookmark extends BaseModel
 {
-    use SoftDeletes;
+    use SoftDeletes , BookmarkEloquent;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'post_id',
+    ];
 
     protected $casts = [];
 }
