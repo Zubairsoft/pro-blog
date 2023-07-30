038e7a1de7168d01eda710c007a5e2fb17c50a38
Mohammed Zubair <moh.775123554@gmail.com>
2023-07-30 22:10:10 +0300

    Add translate attribute

---
+++ app/Domains/Supports/Traits/TranslateAttributes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Domains\Supports\Traits;
+
+use Domains\Supports\Enums\LocalEnum;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\App;
+
+trait TranslateAttributes
+{
+
+
+}
