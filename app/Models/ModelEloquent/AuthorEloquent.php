1090bafc9b217b0ea53e22a64ef1c0bafa99acf3
Mohammed Zubair <moh.775123554@gmail.com>
2023-08-19 12:23:58 +0300

    Update author and admin elqound add comment function

--- app/Models/ModelEloquent/AuthorEloquent.php
+++ app/Models/ModelEloquent/AuthorEloquent.php
@@ -3,6 +3,7 @@
 namespace App\Models\ModelEloquent;
 
 use App\Models\Bookmark;
+use App\Models\Comment;
 use App\Models\OtpActivation;
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
@@ -23,5 +24,10 @@ trait AuthorEloquent
     public function bookmarks(): MorphMany
     {
         return $this->morphMany(Bookmark::class, 'userable');
+    }
+
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Comment::class, 'userable');
     }
 }
