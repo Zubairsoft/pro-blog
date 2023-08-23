102e94f754737c061c3df01b4b985a1615c3c289
Mohammed Zubair <moh.775123554@gmail.com>
2023-08-23 21:52:16 +0300

    Update comment controller add show function with handle functionality

--- app/Http/Controllers/Api/v1/Authors/CommentController.php
+++ app/Http/Controllers/Api/v1/Authors/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1\Authors;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Authors\CommentRequest;
 use Domains\Authors\Actions\Comments\IndexCommentAction;
+use Domains\Authors\Actions\Comments\ShowCommentAction;
 use Domains\Authors\Actions\Comments\StoreCommentAction;
 use Domains\Authors\Actions\Comments\UpdateCommentAction;
 use Illuminate\Http\Request;
@@ -18,17 +19,24 @@ class CommentController extends Controller
         return sendSuccessResponse(__('messages.get_data'), $comments);
     }
 
-    public function store(CommentRequest $request,string $id)
+    public function store(CommentRequest $request, string $id)
     {
         $comments = (new StoreCommentAction)($request, $id);
 
         return sendSuccessResponse(__('messages.create_data'), $comments);
     }
 
-    public function update(CommentRequest $request,string $id,string $commentId)
+    public function show(string $id, string $commentId)
     {
-        $comments = (new UpdateCommentAction)($request, $id,$commentId);
+        $comments = (new ShowCommentAction)($id, $commentId);
 
-        return sendSuccessResponse(__('messages.create_data'), $comments);
+        return sendSuccessResponse(__('messages.get_data'), $comments);
+    }
+
+    public function update(CommentRequest $request, string $id, string $commentId)
+    {
+        $comments = (new UpdateCommentAction)($request, $id, $commentId);
+
+        return sendSuccessResponse(__('messages.update_data'), $comments);
     }
 }
