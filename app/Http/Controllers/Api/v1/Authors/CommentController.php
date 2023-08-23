f44575ed851b4e1f1aca3520218b32b7faf08ea7
Mohammed Zubair <moh.775123554@gmail.com>
2023-08-23 21:57:30 +0300

    Add type hint

--- app/Http/Controllers/Api/v1/Authors/CommentController.php
+++ app/Http/Controllers/Api/v1/Authors/CommentController.php
@@ -9,39 +9,40 @@ use Domains\Authors\Actions\Comments\IndexCommentAction;
 use Domains\Authors\Actions\Comments\ShowCommentAction;
 use Domains\Authors\Actions\Comments\StoreCommentAction;
 use Domains\Authors\Actions\Comments\UpdateCommentAction;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function index(Request $request, string $id)
+    public function index(Request $request, string $id): JsonResponse
     {
         $comments = (new IndexCommentAction)($request, $id);
 
         return sendSuccessResponse(__('messages.get_data'), $comments);
     }
 
-    public function store(CommentRequest $request, string $id)
+    public function store(CommentRequest $request, string $id): JsonResponse
     {
         $comments = (new StoreCommentAction)($request, $id);
 
         return sendSuccessResponse(__('messages.create_data'), $comments);
     }
 
-    public function show(string $id, string $commentId)
+    public function show(string $id, string $commentId): JsonResponse
     {
         $comments = (new ShowCommentAction)($id, $commentId);
 
         return sendSuccessResponse(__('messages.get_data'), $comments);
     }
 
-    public function update(CommentRequest $request, string $id, string $commentId)
+    public function update(CommentRequest $request, string $id, string $commentId): JsonResponse
     {
         $comments = (new UpdateCommentAction)($request, $id, $commentId);
 
         return sendSuccessResponse(__('messages.update_data'), $comments);
     }
 
-    public function destroy(CommentRequest $request, string $id)
+    public function destroy(CommentRequest $request, string $id): JsonResponse
     {
         $comments = (new DestroyCommentAction)($request, $id);
 
