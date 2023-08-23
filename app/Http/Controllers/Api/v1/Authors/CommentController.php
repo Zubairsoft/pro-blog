41b47a2762c8515e80160171b60ae43458807d46
Mohammed Zubair <moh.775123554@gmail.com>
2023-08-23 21:33:05 +0300

    Update comment controller add index

--- app/Http/Controllers/Api/v1/Authors/CommentController.php
+++ app/Http/Controllers/Api/v1/Authors/CommentController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api\v1\Authors;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Request;
 
 class CommentController extends Controller
 {
+    public function index(Request $request,string $id)
+    {
 
-   
+    }
 }
