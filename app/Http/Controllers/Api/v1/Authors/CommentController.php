6030549b1de1ebc265ed4d71580fdf33cebc0e51
Mohammed Zubair <moh.775123554@gmail.com>
2023-08-23 21:45:15 +0300

    Update comment controller add store function with handle functionality

--- app/Http/Controllers/Api/v1/Authors/CommentController.php
+++ app/Http/Controllers/Api/v1/Authors/CommentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\v1\Authors;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Authors\CommentRequest;
 use Domains\Authors\Actions\Comments\IndexCommentAction;
+use Domains\Authors\Actions\Comments\StoreCommentAction;
 use GuzzleHttp\Psr7\Request;
 
 class CommentController extends Controller
@@ -13,5 +15,12 @@ class CommentController extends Controller
         $comments = (new IndexCommentAction)($request, $id);
 
         return sendSuccessResponse(__('messages.get_data'), $comments);
+    }
+
+    public function store(CommentRequest $request,string $id)
+    {
+        $comments = (new StoreCommentAction)($request, $id);
+
+        return sendSuccessResponse(__('messages.create_data'), $comments);
     }
 }
