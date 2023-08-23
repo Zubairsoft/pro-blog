ef11dfb7776fa9085fe91532d5d5c5da5d4ffb0b
Mohammed Zubair <moh.775123554@gmail.com>
2023-08-23 21:41:21 +0300

    Update comment controller update the index function handle functionality

--- app/Http/Controllers/Api/v1/Authors/CommentController.php
+++ app/Http/Controllers/Api/v1/Authors/CommentController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api\v1\Authors;
 
 use App\Http\Controllers\Controller;
+use Domains\Authors\Actions\Comments\IndexCommentAction;
 use GuzzleHttp\Psr7\Request;
 
 class CommentController extends Controller
 {
-    public function index(Request $request,string $id)
+    public function index(Request $request, string $id)
     {
+        $comments = (new IndexCommentAction)($request, $id);
 
+        return sendSuccessResponse(__('messages.get_data'), $comments);
     }
 }
