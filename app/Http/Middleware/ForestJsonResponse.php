3f3d81c8d9692b4243dfd1be38fe9b1c677fa9e4
Mohammed Zubair <moh.775123554@gmail.com>
2023-08-31 13:25:25 +0300

    Update middleware handle functionality

--- app/Http/Middleware/ForestJsonResponse.php
+++ app/Http/Middleware/ForestJsonResponse.php
@@ -8,14 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ForestJsonResponse
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-      //    $request->header()->set(['type'=>'Application/Json']);
-        return $next($request);
-    }
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+   */
+  public function handle(Request $request, Closure $next): Response
+  {
+    $request->headers->set('Accept', 'Application/Json');
+    return $next($request);
+  }
 }
