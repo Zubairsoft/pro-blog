45be3df75e5014f58cfe397a98377cf7433845da
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-02 18:56:51 +0300

    Update reply comment policy with handle functionality

--- app/Policies/ReplyCommentPolicy.php
+++ app/Policies/ReplyCommentPolicy.php
@@ -5,62 +5,28 @@ namespace App\Policies;
 use App\Models\ReplyComment;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
 
 class ReplyCommentPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, ReplyComment $replyComment): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, ReplyComment $replyComment): bool
+    public function update(ReplyComment $replyComment): bool
     {
-        //
+        $user = Auth::user();
+
+        return $user->id === $replyComment->user_id;
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, ReplyComment $replyComment): bool
+    public function destroy(User $user, ReplyComment $replyComment): bool
     {
-        //
-    }
+        $user = Auth::user();
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, ReplyComment $replyComment): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, ReplyComment $replyComment): bool
-    {
-        //
+        return $user->id === $replyComment->user_id;
     }
 }
