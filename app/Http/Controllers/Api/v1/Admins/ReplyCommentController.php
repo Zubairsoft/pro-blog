129db8d2bb2f781cf744cf3d0525ec9570ed7e27
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-02 14:56:24 +0300

    Add reply comment controller

---
+++ app/Http/Controllers/Api/v1/Admins/ReplyCommentController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Admins;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admins\ReplyCommentRequest;
+
+class ReplyCommentController extends Controller
+{
+    public function store(ReplyCommentRequest $request, string $id, string $commentId)
+    {
+
+    }
+
+    public function update(ReplyCommentRequest $request, string $id, string $commentId,string $replyCommentId)
+    {
+
+    }
+}
