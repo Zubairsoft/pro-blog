83d19620ea1f1a8b827334b21b7020edfe07b65c
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-02 15:25:57 +0300

    Update replay controller handle functionality

--- app/Http/Controllers/Api/v1/Admins/ReplyCommentController.php
+++ app/Http/Controllers/Api/v1/Admins/ReplyCommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1\Admins;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admins\ReplyCommentRequest;
 use Domains\Admins\Actions\ReplyComments\StoreReplyCommentAction;
+use Domains\Admins\Actions\ReplyComments\UpdateReplyCommentAction;
 use Illuminate\Http\JsonResponse;
 
 class ReplyCommentController extends Controller
@@ -25,7 +26,20 @@ class ReplyCommentController extends Controller
         return sendSuccessResponse(__('messages.create_data'), $replyComment);
     }
 
-    public function update(ReplyCommentRequest $request, string $id, string $commentId, string $replyCommentId)
+    /**
+     * Handle the incoming request for Update replay comment
+     * 
+     * @param ReplyCommentRequest $request
+     * @param string $id
+     * @param string $commentId
+     * @param string $replyCommentId
+     * 
+     * @return JsonResponse
+     */
+    public function update(ReplyCommentRequest $request, string $id, string $commentId, string $replyCommentId): JsonResponse
     {
+        $replyComment = (new UpdateReplyCommentAction)($request, $id, $commentId,$replyCommentId);
+
+        return sendSuccessResponse(__('messages.update_data'), $replyComment);
     }
 }
