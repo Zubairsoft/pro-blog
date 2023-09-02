7bc92b9f214eaeb48995eaa16f8d967642d9390e
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-02 15:16:46 +0300

    Update replay comment controller handle store functionality

--- app/Http/Controllers/Api/v1/Admins/ReplyCommentController.php
+++ app/Http/Controllers/Api/v1/Admins/ReplyCommentController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers\Api\v1\Admins;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admins\ReplyCommentRequest;
+use Domains\Admins\Actions\ReplyComments\StoreReplyCommentAction;
+use Illuminate\Http\JsonResponse;
 
 class ReplyCommentController extends Controller
 {
-    public function store(ReplyCommentRequest $request, string $id, string $commentId)
+    /**
+     * Handle the incoming request for store reply comment
+     * 
+     * @param ReplyCommentRequest $request
+     * @param string $id
+     * @param string $commentId
+     * 
+     * @return JsonResponse
+     */
+    public function store(ReplyCommentRequest $request, string $id, string $commentId): JsonResponse
     {
+        $replyComment = (new StoreReplyCommentAction)($request, $id, $commentId);
 
+        return sendSuccessResponse(__('messages.create_data'), $replyComment);
     }
 
-    public function update(ReplyCommentRequest $request, string $id, string $commentId,string $replyCommentId)
+    public function update(ReplyCommentRequest $request, string $id, string $commentId, string $replyCommentId)
     {
-
     }
 }
