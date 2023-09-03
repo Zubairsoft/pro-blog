4d92f800f927b785937ae771a6a871289f318671
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-03 20:36:55 +0300

    Add reply comment controller

---
+++ app/Http/Controllers/Api/v1/Authors/ReplyCommentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Authors;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReplyCommentController extends Controller
+{
+    //
+}
