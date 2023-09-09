4b18ab04077c3b1935c6193a82f46e7037f08c8b
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-09 14:28:42 +0300

    Update admin controller handle the crud functionality

--- app/Http/Controllers/Api/v1/Admins/AdminController.php
+++ app/Http/Controllers/Api/v1/Admins/AdminController.php
@@ -3,8 +3,84 @@
 namespace App\Http\Controllers\Api\v1\Admins;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admins\AdminRequest;
+use Domains\Admins\Actions\Admins\DestroyAdminAction;
+use Domains\Admins\Actions\Admins\IndexAdminAction;
+use Domains\Admins\Actions\Admins\ShowAdminAction;
+use Domains\Admins\Actions\Admins\StoreAdminAction;
+use Domains\Admins\Actions\Admins\UpdateAdminAction;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    /**
+     * Handel the incoming request for index admin
+     * 
+     * @param Request $request
+     * 
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        $admins = (new IndexAdminAction)($request);
 
+        return sendSuccessResponse(__('messages.get_data'), $admins);
+    }
+
+    /**
+     * Handle the incoming request for store admin
+     * 
+     * @param AdminRequest $request
+     * 
+     * @return JsonResponse
+     */
+    public function store(AdminRequest $request): JsonResponse
+    {
+        $admin = (new StoreAdminAction)($request);
+
+        return sendSuccessResponse(__('messages.create_data'), $admin);
+    }
+
+    /**
+     * Handle the incoming request for show admin
+     * 
+     * @param string $id
+     * 
+     * @return JsonResponse
+     */
+    public function show(string $id): JsonResponse
+    {
+        $admin = (new ShowAdminAction())($id);
+
+        return sendSuccessResponse(__('messages.get_data'), $admin);
+    }
+
+    /**
+     * Handle the incoming request for update admin
+     * 
+     * @param AdminRequest $request
+     * 
+     * @return JsonResponse
+     */
+    public function update(AdminRequest $request, string $id): JsonResponse
+    {
+        $admin = (new UpdateAdminAction)($request, $id);
+
+        return sendSuccessResponse(__('messages.create_data'), $admin);
+    }
+
+    /**
+     * Handle the incoming request for delete admin
+     * 
+     * @param AdminRequest $request
+     * 
+     * @return JsonResponse
+     */
+    public function  destroy(AdminRequest $request): JsonResponse
+    {
+        $admin = (new DestroyAdminAction)($request);
+
+        return sendSuccessResponse(__('messages.create_data'), $admin);
+    }
 }
