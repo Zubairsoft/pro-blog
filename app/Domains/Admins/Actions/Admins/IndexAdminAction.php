e100541af5177aad5f08493890747a905987e52d
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-09 15:42:45 +0300

    Update index admin action add gender filter

--- app/Domains/Admins/Actions/Admins/IndexAdminAction.php
+++ app/Domains/Admins/Actions/Admins/IndexAdminAction.php
@@ -3,6 +3,7 @@
 namespace Domains\Admins\Actions\Admins;
 
 use App\Models\Admin;
+use Domains\Supports\Enums\GenderEnum;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
@@ -14,13 +15,19 @@ final class IndexAdminAction
 
         $searchText = $request->input('searchText');
 
+        $gender = $request->input('gender') ? (in_array($request->gender, GenderEnum::getKeys()) ? $request->gender : false) : false;
+
         $sortByName = $request->input('sortByName') ?? 'asc';
 
         $sortBy = $request->input('sortBy') ?? 'desc';
 
         $query = Admin::query()->when(
             $searchText,
-            fn (Builder $builder) => $builder->search($searchText)
+            fn (Builder $builder) => $builder->search(['name', 'email'], $searchText)
+        )->when(
+            $gender,
+            fn (Builder $builder) =>
+            $builder->where('gender', $request->gender)
         )
             ->orderBy('created_at', $sortBy)
             ->orderBy('name', $sortByName)
