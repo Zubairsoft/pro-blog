7305ac929f6fc218941c07fa38c3ca0377c95071
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-09 14:48:15 +0300

    fix name

--- app/Http/Controllers/Api/v1/Admins/AuthorController.php
+++ app/Http/Controllers/Api/v1/Admins/AuthorController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\v1\Admins;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AuthorContorller extends Controller
+class AuthorController extends Controller
 {
     //
 }
