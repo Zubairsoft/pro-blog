0748f872464330d3b25fa1b5865dd86f02d8f711
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-13 12:03:51 +0300

    Add author resource

---
+++ app/Http/Resources/Admins/AuthorResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Admins;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'gender' => $this->gender_translate,
+            'local' => $this->local,
+            'is_active' => $this->is_active
+        ];
+    }
+}
