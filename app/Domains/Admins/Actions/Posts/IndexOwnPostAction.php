2308b92a772a7a973ceb8a1c7325ccd4f7304436
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-17 20:38:39 +0300

    Load tags and media

--- app/Domains/Admins/Actions/Posts/IndexOwnPostAction.php
+++ app/Domains/Admins/Actions/Posts/IndexOwnPostAction.php
@@ -2,6 +2,7 @@
 
 namespace Domains\Admins\Actions\Posts;
 
+use App\Http\Resources\Admins\Posts\PostResource;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,13 +21,13 @@ class IndexOwnPostAction
 
         $admin = Auth::user();
 
-        $query = $admin->posts()
+        $posts = $admin->posts()->with(['tags','media'])
             ->when(
                 $searchText,
                 fn (Builder $builder) => $builder->search(['title_ar', 'title_en', 'description_ar', 'description_en'], $searchText)
             )->orderBy($sort, $sortBy)
             ->paginate($perPage);
 
-        return $query;
-    }
+            return PostResource::collection($posts)->appends($request->query())->toArray();
+        }
 }
