85cd58a6826326a33a0c23a3062fe011b7145fca
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-17 21:02:13 +0300

    Update has media form request trait handle mulible images

--- app/Domains/Supports/Traits/HasMediaFromRequest.php
+++ app/Domains/Supports/Traits/HasMediaFromRequest.php
@@ -7,7 +7,20 @@ trait HasMediaFromRequest
     public function AddImageFromRequestIfExists($request, $file, $collection)
     {
         if ($request->hasFile($file)) {
-            $this->addMediaFromRequest($file)->toMediaCollection($collection);
+            $this->addMediaFromRequest($file)
+                ->toMediaCollection($collection);
+        }
+    }
+
+    public function AddMultipleImageFromRequestIfRequestIfExists($request, $file, $collection)
+    {
+        if ($request->has($file)) {
+            $this->addMultipleMediaFromRequest([$file])
+                ->each(
+                    fn ($fileAdder) =>
+                    $fileAdder
+                        ->toMediaCollection($collection)
+                );
         }
     }
 }
