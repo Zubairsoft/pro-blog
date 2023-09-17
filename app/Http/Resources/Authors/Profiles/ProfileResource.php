47df28fffe523a99860652ad32637e353348679f
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-17 21:04:08 +0300

    Add resources

---
+++ app/Http/Resources/Authors/Profiles/ProfileResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Authors\Profiles;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'gender' => $this->gender_translate,
+            'local' => $this->local,
+            'avatar' => $this->avatar
+        ];
+    }
+}
