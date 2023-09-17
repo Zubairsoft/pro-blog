3800b6c4d08b9946150107864989e0981796c273
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-17 21:08:58 +0300

    Handle post poster and images

--- app/Models/ModelAttributes/PostAttributes.php
+++ app/Models/ModelAttributes/PostAttributes.php
@@ -2,9 +2,27 @@
 
 namespace App\Models\ModelAttributes;
 
-use Domains\Supports\Traits\TranslateAttributes;
+use Domains\Supports\Traits\CommonAttributes\TranslateAttributes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 trait PostAttributes
 {
     use TranslateAttributes;
+
+
+    protected function poster(): Attribute
+    {
+        return Attribute::make(
+            get: fn () =>
+            $this->getFirstMediaUrl('poster')
+        );
+    }
+
+    protected function images(): Attribute
+    {
+        return Attribute::make(
+            get: fn () =>
+            $this->getMedia('post-images')
+        );
+    }
 }
