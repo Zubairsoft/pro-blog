a1db1e54a04c8e25b5e10f4602593c71aa00bbaf
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-20 15:58:23 +0300

    Update user eloquent handle functionality

--- app/Models/ModelEloquent/UserEloquent.php
+++ app/Models/ModelEloquent/UserEloquent.php
@@ -3,6 +3,7 @@
 namespace App\Models\ModelEloquent;
 
 use App\Models\OtpActivation;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 trait UserEloquent
@@ -10,5 +11,25 @@ trait UserEloquent
     public function otpActivation(): MorphOne
     {
         return $this->morphOne(OtpActivation::class, 'activatable');
+    }
+
+    public function posts(): MorphMany
+    {
+        return $this->morphMany(Post::class, 'authorable');
+    }
+
+    public function bookmarks(): MorphMany
+    {
+        return $this->morphMany(Bookmark::class, 'userable');
+    }
+
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Comment::class, 'userable');
+    }
+
+    public function replyComments(): MorphMany
+    {
+        return $this->morphMany(ReplyComment::class, 'user');
     }
 }
