a68b7baed080e70042abb14a3cda03e9ea968f72
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-20 16:00:20 +0300

    Update route service provider add supports route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,6 +34,7 @@ class RouteServiceProvider extends ServiceProvider
             $this->routeAdminMap();
             $this->routeAuthorMap();
             $this->routeWebMap();
+            $this->routeSupportMap();
         });
     }
 
