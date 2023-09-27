6de3d82ff16a4931587fd0e545785ac469e83e5e
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-27 13:10:24 +0300

    edit endpoint

--- routes/support.php
+++ routes/support.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\Api\v1\Supports\ClearImages\ClearPosterController;
 use App\Http\Controllers\Api\v1\Supports\ClearImages\ClearPostImagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('clear_images.')->prefix('clear-images')->group(function () {
+Route::name('clear_images.')->middleware('auth:sanctum')->prefix('clear-images')->group(function () {
 
-    Route::get('avatar', ClearAvatarController::class)->name('avatar');
-    Route::get('poster/{id}', ClearPosterController::class)->name('poster');
-    Route::get('post-images/{id}', ClearPostImagesController::class)->name('post_images');
+    Route::delete('avatar', ClearAvatarController::class)->name('avatar');
+    Route::delete('post/{id}/poster', ClearPosterController::class)->name('poster');
+    Route::delete('post/{id}/post-images', ClearPostImagesController::class)->name('post_images');
 });
