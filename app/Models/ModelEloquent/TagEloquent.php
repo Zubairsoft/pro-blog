7c473bb04f9c70c9a2920d449fb8d32f3c322421
Mohammed Zubair <moh.775123554@gmail.com>
2023-09-27 13:06:34 +0300

    Add admin relationship

--- app/Models/ModelEloquent/TagEloquent.php
+++ app/Models/ModelEloquent/TagEloquent.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\ModelEloquent;
 
+use App\Models\Admin;
 use App\Models\Post;
 use App\Models\TagPost;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -11,5 +12,10 @@ trait TagEloquent
     public function posts(): BelongsToMany
     {
         return $this->belongsToMany(Post::class,'tag_posts')->using(TagPost::class);
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo(Admin::class);
     }
 }
