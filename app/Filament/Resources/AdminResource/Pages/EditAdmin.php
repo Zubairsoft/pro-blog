db8faa3f3fdc4261b40364a17502a1d6644c7b12
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-02 10:23:32 +0300

    Update edit admin action

--- app/Filament/Resources/AdminResource/Pages/EditAdmin.php
+++ app/Filament/Resources/AdminResource/Pages/EditAdmin.php
@@ -3,12 +3,28 @@
 namespace App\Filament\Resources\AdminResource\Pages;
 
 use App\Filament\Resources\AdminResource;
+use App\Filament\Resources\Supports\RedirectToThePreviousPage;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditAdmin extends EditRecord
 {
+    use RedirectToThePreviousPage;
+
     protected static string $resource = AdminResource::class;
+
+    protected function handleRecordUpdate(Model $admin, array $data): Model
+    {
+        $admin->update($data);
+        
+        return $admin;
+    }
+
+    protected function getSavedNotificationTitle(): ?string
+    {
+        return __('messages.update_data');
+    }
 
     protected function getHeaderActions(): array
     {
