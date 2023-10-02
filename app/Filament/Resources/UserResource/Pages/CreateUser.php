d0f080744e52e12e86b1d7b5eec0f9d0d0f2c04a
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-02 10:32:21 +0300

    Update create user action

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -2,11 +2,25 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Filament\Resources\Supports\RedirectToThePreviousPage;
 use App\Filament\Resources\UserResource;
+use Domains\Supports\Enums\RoleEnum;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateUser extends CreateRecord
 {
+    use RedirectToThePreviousPage;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $user = static::getModel()::query()->create($data);
+
+        $user->assignRole(RoleEnum::USER);
+
+        return $user;
+    }
+
     protected static string $resource = UserResource::class;
 }
