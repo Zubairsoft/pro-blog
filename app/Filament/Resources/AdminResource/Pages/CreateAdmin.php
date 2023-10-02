77851183fc534ff279f0eea6859c45c1bcd81305
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-02 10:21:13 +0300

    Update create admin action

--- app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
+++ app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
@@ -3,10 +3,29 @@
 namespace App\Filament\Resources\AdminResource\Pages;
 
 use App\Filament\Resources\AdminResource;
+use App\Filament\Resources\Supports\RedirectToThePreviousPage;
+use Domains\Supports\Enums\RoleEnum;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateAdmin extends CreateRecord
 {
+    use RedirectToThePreviousPage;
+
     protected static string $resource = AdminResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $admin = static::getModel()::query()->create($data);
+
+        $admin->assignRole(RoleEnum::ADMIN);
+
+        if (is_file($data['avatar'])) {
+            
+            $admin->addMedia($data['avatar'])->toMediaCollection('avatar');
+        }
+
+        return $admin;
+    }
 }
