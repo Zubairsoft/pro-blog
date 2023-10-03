a6b14e6bd1d0723e281043dca3ee5668f8a3c8e4
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-03 11:51:57 +0300

    Add report model with handle functionality

---
+++ app/Models/Report.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\ModelAttributes\ReportAttributes;
+use App\Models\ModelEloquent\ReportEloquent;
+use Domains\Admins\Enums\ReportStatusEnum;
+use Domains\Supports\Traits\HasSearch;
+
+class Report extends BaseModel
+{
+    use HasSearch;
+    use ReportEloquent;
+    use ReportAttributes;
+
+    protected $fillable = [
+        'reason',
+    ];
+
+    protected $casts = [
+        'status' => ReportStatusEnum::class
+    ];
+}
