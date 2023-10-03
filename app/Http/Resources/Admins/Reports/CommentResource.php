641272fbf1b929a2fedd98ae0b13d9e6a5ade362
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-03 11:57:19 +0300

    Add report resources with handle functionality

---
+++ app/Http/Resources/Admins/Reports/CommentResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Admins\Reports;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'comment' => $this->comment,
+            'writer' => $this->userable->name
+        ];
+    }
+}
