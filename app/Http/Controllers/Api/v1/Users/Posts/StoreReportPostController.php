84e25abca55aa3ba9f7329c14e824ab6ca13e1c7
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-04 17:02:43 +0300

    Add store report post controller

---
+++ app/Http/Controllers/Api/v1/Users/Posts/StoreReportPostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Users\Posts;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Users\Posts\StoreReportPostRequest;
+use Illuminate\Http\Request;
+
+class StoreReportPostController extends Controller
+{
+    public function __invoke(StoreReportPostRequest $request)
+    {
+
+    }
+}
