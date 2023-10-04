dd47e5de58de45fe14acb5e1f27371cdcb96caf1
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-04 23:26:34 +0300

    Update controller handle functionality

--- app/Http/Controllers/Api/v1/Users/Posts/StoreReportPostController.php
+++ app/Http/Controllers/Api/v1/Users/Posts/StoreReportPostController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers\Api\v1\Users\Posts;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Users\Posts\StoreReportPostRequest;
-use Illuminate\Http\Request;
+use Domains\Users\Actions\Posts\StoreReportPostAction;
+use Illuminate\Http\JsonResponse;
 
 class StoreReportPostController extends Controller
 {
-    public function __invoke(StoreReportPostRequest $request)
+    /**
+     * Handle the incoming request for report post 
+     * 
+     * @param StoreReportPostRequest $request
+     * 
+     * @return JsonResponse
+     */
+    public function __invoke(StoreReportPostRequest $request,string $id):JsonResponse
     {
+        (new StoreReportPostAction)($request,$id);
 
+        return sendSuccessResponse(__('messages.create_data'));
     }
 }
