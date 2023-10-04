745600fd01515eb14a401fd0d5c40945b0b68ce0
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-04 23:25:23 +0300

    Update store report post handle functionality

--- app/Domains/Users/Actions/Posts/Reports/StoreReportPostAction.php
+++ app/Domains/Users/Actions/Posts/Reports/StoreReportPostAction.php
@@ -2,7 +2,25 @@
 
 namespace Domains\Users\Actions\Posts;
 
+use App\Exceptions\CustomException\LogicException;
+use App\Http\Requests\Users\Posts\StoreReportPostRequest;
+use App\Models\Post;
+use Domains\Users\DataTransferToObject\ReportData;
+
 class StoreReportPostAction
 {
+    public function __invoke(StoreReportPostRequest $request, string $id)
+    {
+        $user = getAuthenticatedUser();
 
+        $attributes = unsetArrayEmptyParam(ReportData::fromRequest($request)->toArray());
+
+        $post = Post::query()->active()->findOrFail($id);
+
+        if ($user->id === $post->authorable_id) {
+            throw new LogicException(__('exceptions.self_post_report'), 422);
+        }
+
+        $post->reports()->create($attributes);
+    }
 }
