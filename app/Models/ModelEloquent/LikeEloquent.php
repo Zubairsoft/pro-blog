f4ffb7dcc4067a01b96d17540a039d47e32103b9
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-05 16:35:57 +0300

    Add like eloquent

---
+++ app/Models/ModelEloquent/LikeEloquent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\ModelEloquent;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+trait LikeEloquent
+{
+    public function userable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
