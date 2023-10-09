07342a589982655b685c96b55923723b55b56aa4
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-09 13:32:43 +0300

    Add report data with handle functionality

---
+++ app/Domains/Supports/DataTransferToObject/ReportData.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Domains\Supports\DataTransferToObject;
+
+use Spatie\LaravelData\Data;
+
+final class ReportData extends Data
+{
+
+    public function __construct(
+        public ?string $writable_id,
+        public ?string $writable_type,
+        public ?string $reason,
+    ) {
+    }
+
+    public static function fromRequest($request): ReportData
+    {
+        $user = getAuthenticatedUser();
+
+        return new self(
+            $user->id,
+            get_class($user),
+            $request->post('reason'),
+        );
+    }
+}
