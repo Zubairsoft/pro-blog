a44ed645ec0e34e7b4256a08b57f5cb44d3fb7d8
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-11 12:01:37 +0300

    Add profile request

---
+++ app/Http/Requests/Users/Sessions/ProfileRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Users\Sessions;
+
+use Domains\Supports\Enums\GenderEnum;
+use Domains\Supports\Enums\LocalEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+    return [];
+}
+}
