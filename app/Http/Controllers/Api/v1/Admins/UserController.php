30852a22cf3747e6c3e74d979691f564717d41b9
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-11 16:35:58 +0300

    Add user controller

---
+++ app/Http/Controllers/Api/v1/Admins/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Admins;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admins\UserRequest;
+use App\Http\Resources\Admins\UserResource;
+use Domains\Admins\Actions\Users\DestroyUserAction;
+use Domains\Admins\Actions\Users\IndexUserAction;
+use Domains\Admins\Actions\Users\ShowUserAction;
+use Domains\Admins\Actions\Users\StoreUserAction;
+use Domains\Admins\Actions\Users\UpdateUserAction;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+}
