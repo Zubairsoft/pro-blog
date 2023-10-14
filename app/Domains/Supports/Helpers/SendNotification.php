c6b80442fa9d2133edec0527a8cead5a2f1aaa7d
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-14 19:36:40 +0300

    Update send notification

--- app/Domains/Supports/Helpers/SendNotification.php
+++ app/Domains/Supports/Helpers/SendNotification.php
@@ -3,7 +3,9 @@
 namespace Domains\Supports\Helpers;
 
 use App\Models\Admin;
+use Domains\Supports\Enums\RoleEnum;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Notification as FacadesNotification;
 
 class SendNotification
 {
@@ -11,8 +13,13 @@ class SendNotification
     {
         $admins = Admin::query()->active()->get();
 
-        foreach ($admins as $admin) {
-            $admin->notify($notification);
-        }
+        FacadesNotification::send($admins, $notification);
+    }
+
+    public static function toSuperAdmin(Notification $notification)
+    {
+        $admin = Admin::query()->role(RoleEnum::SUPER_ADMIN)->active()->firstOrFail();
+
+        $admin->notify($notification);
     }
 }
