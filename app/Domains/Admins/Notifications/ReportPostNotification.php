ddf8242d849b4998d7d20592644a71f9c8ea61ba
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-14 19:21:57 +0300

    Add report post notification with handle notification

---
+++ app/Domains/Admins/Notifications/ReportPostNotification.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Domains\Admins\Notifications;
+
+use App\Models\Report;
+use Domains\Supports\Enums\UserEnum;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+
+class ReportPostNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public Report $report)
+    {
+        $this->onQueue('notification');
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toDatabase(): array
+    {
+        return [
+            'type' => 'REPORT',
+            'title_ar' => __('notifications.report_post.title', [], 'ar'),
+            'title_en' => __('notifications.report_post.title', [], 'en'),
+            'body_ar' => __('notifications.report_post.body', ['name' => $this->report->writable->name], 'ar'),
+            'body_en' => __('notifications.report_post.body', ['name' => $this->report->writable->name], 'en'),
+            'id' => $this->report->id,
+        ];
+    }
+}
