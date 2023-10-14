4dab7c482011e7bf665625ba6771ff33d4584737
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-14 20:20:41 +0300

    Add Index Author notification with handle functionality

---
+++ app/Domains/Authors/Actions/Notifications/IndexAuthorNotificationAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Domains\Authors\Actions\Notifications;
+
+use Illuminate\Support\Facades\Auth;
+
+final class IndexAuthorNotificationAction
+{
+    public function __invoke()
+    {
+        $author = Auth::guard(config('auth.author-api'))->user();
+
+        return $author->load('notifications');
+    }
+}
