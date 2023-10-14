2bbd595111a1750bc2feedfd68bdeaa083870c94
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-14 20:20:11 +0300

    Add index Notification action with handle functionality

---
+++ app/Domains/Admins/Actions/Notifications/IndexAdminNotificationAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Domains\Admins\Actions\Notifications;
+
+use Illuminate\Support\Facades\Auth;
+
+final class IndexAdminNotificationAction
+{
+    public function __invoke()
+    {
+        $admin = Auth::guard(config('auth.admin-api-guard'))->user();
+
+        return $admin->load('notifications');
+    }
+}
