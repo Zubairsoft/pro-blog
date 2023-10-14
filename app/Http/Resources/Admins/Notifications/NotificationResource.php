0ecf57421c8d80955180689681a64ae28af558df
Mohammed Zubair <moh.775123554@gmail.com>
2023-10-14 22:49:34 +0300

    Add notification resource

---
+++ app/Http/Resources/Admins/Notifications/NotificationResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Admins\Notifications;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NotificationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'unreadNotificationsCount'=>$this->unread_notifications_count,
+            'notifications'=>NotificationDataResource::collection($this->notifications)
+            
+        ];
+    }
+}
